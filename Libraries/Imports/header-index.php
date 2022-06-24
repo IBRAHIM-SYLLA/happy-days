ec16f17416e8edb086b1aea46a80e6372885fa7d
Thibault <thibault.kine@laplateforme.io>
2022-06-24 17:51:28 +0200

    menu burger en cours

--- Libraries/Imports/header-index.php
+++ Libraries/Imports/header-index.php
@@ -6,20 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" href="./Libraries/Style/style.css">
-
     <?php
     // vérifie si l'user est sur mobile ou non
     function isMobile() {
         return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
     }
+
+    isMobile() ? $cssDIR = "./Libraries/Style/CSS/Mobile/" : $cssDIR = "./Libraries/Style/CSS/Computer/";
     ?>
+
+    <link rel="stylesheet" href="<?= $cssDIR ?>style.css">
 </head>
 <body>
 
 <header>
-    <!-- Header ici -->
-    <h1>Header</h1>
+    <a href="index.php"><img src="./Libraries/Style/Images/logo.png" class="logo"></a>
 </header>
+
+<div id="sub-header">
+    <a href="./Libraries/Views/presentation.php">Qui sommes-nous ?</a>
+    <a href="./Libraries/Views/nos-actions.php">Nos actions</a>
+    <a href="./Libraries/Views/adherer.php">Adhérer</a>
+    <a href="./Libraries/Views/contact.php">Contact</a>
+</div>
 
 <main>
--- Libraries/Imports/header.php
+++ Libraries/Imports/header.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" href="../Style/style.css">
-
     <script src="../Scripts/form-tabs-manager.js"></script>
 
     <?php
@@ -15,13 +13,16 @@
     function isMobile() {
         return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
     }
+    isMobile() ? $cssDIR = "./Libraries/Style/CSS/Mobile/" : $cssDIR = "./Libraries/Style/CSS/Computer/";
     ?>
+
+    <link rel="stylesheet" href="<?= $cssDIR ?>style.css">
 </head>
 <body>
 
 <header>
-    <!-- Header ici -->
-    <h1>Header</h1>
+    <a href="index.php" style="visibility:hidden;"><img src="./Libraries/Style/Images/logo.png" id="logo"></a>
 </header>
+
 
 <main>
