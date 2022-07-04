44aa367677f189615031d51b4fd95e50dcdf98c2
Thibault <thibault.kine@laplateforme.io>
2022-07-04 13:44:34 +0200

    footer index

--- index.php
+++ index.php
@@ -7,13 +7,12 @@ if (isset($_POST['deconnex'])){
     session_destroy();
     header('Location: connexion.php');
 }
-?>
 
-<?php var_dump($_SESSION) ?>
+var_dump($_SESSION) ?>
 <a href="./Libraries/Views/inscription.php">inscription</a>
 <a href="./Libraries/Views/connexion.php">connexion</a>
 <form action="" method="post">
     <button type="submit" name='deconnex'>deconnexion</button>
 </form>
 
-<?php include_once('./Libraries/Imports/footer.php') ?>
+<?php include_once('./Libraries/Imports/footer-index.php') ?>
