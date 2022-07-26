7be2d59d4056f5c3f8e128ad3e57efc8aa565d24
Sylla Ibrahim <ibrahim-sylla>
2022-07-26 13:22:34 +0200

    page events done

--- Libraries/Imports/header.php
+++ Libraries/Imports/header.php
@@ -26,6 +26,7 @@
     <link rel="stylesheet" href="../Style/CSS/paiement.css">
     <link rel="stylesheet" href="../Style/CSS/confirmation.css">
     <link rel="stylesheet" href="../Style/CSS/profil.css">
+    <link rel="stylesheet" href="../Style/CSS/events.css">
 </head>
 <body>
 
--- Libraries/Views/events.php
+++ Libraries/Views/events.php
@@ -1,24 +1,17 @@
 <?php
 // require_once '../Models/Event.php';
 require_once '../Controllers/event.php';
-// $fetchAllEvents = $event->selectAllEvents();
+require_once '../Imports/header.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<section id="allEvents">
     <?php foreach ($fetchAllEvents as $fetchAllEvent): ?>
+            <div class="event">
+                <img src="../uploads/<?=$fetchAllEvent['image']; ?>" alt="">
+                <h4><?= $fetchAllEvent['name']; ?></h4>
+                <h4><?= $fetchAllEvent['date']; ?></h4>
+                <a href="event.php?id=<?=$fetchAllEvent['id']; ?>"><button>Voir l'event</button></a>
+            </div>
+    <?php endforeach; ?>
+</section>
 
-            <img src="../uploads/<?=$fetchAllEvent['image']; ?>" alt="">
-            <?= $fetchAllEvent['name']; ?>
-            <?= $fetchAllEvent['date']; ?>
-            <a href="event.php?id=<?=$fetchAllEvent['id']; ?>"><button>Voir l'event</button></a>
-        <?php endforeach; ?>
-</body>
-</html>
+<?php include_once '../Imports/footer.php'; ?>
