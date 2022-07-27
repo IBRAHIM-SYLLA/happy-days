cf0a034f32919df169ab48c0f3bc003dd61bee65
Sylla Ibrahim <ibrahim-sylla>
2022-07-27 13:38:05 +0200

    contact security

--- Libraries/Controllers/contact.php
+++ Libraries/Controllers/contact.php
@@ -4,7 +4,20 @@ require_once 'functions.php';
 if(isset($_POST['contact'])){
     if(!empty($_POST["name"]) && !empty($_POST["last-name"]) && !empty($_POST["mail"]) && !empty($_POST["message"])){
 
-        sendMailContact($_POST["name"], $_POST["mail"],$_POST["last-name"], $_POST["message"]);
+        $name = security($_POST["name"]);
+        $lastname = security($_POST["last-name"]);
+        $mail = security($_POST["mail"]);
+        $message = security($_POST["message"]);
+
+        if (preg_match("/^[^@]+@[^@]+\.[a-z]{2,6}$/i", $mail)){
+            sendMailContact($name, $mail,$lastname, $message);
+        }
+        else {
+            echo "L'email n'est pas valide.";
+        }
+    }
+    else {
+        echo "Un des champs est vide.";
     }
 }
 
