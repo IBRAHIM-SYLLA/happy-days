48defa3f1c825cb6a3d3628e58bb4a97c63a2331
Thibault <thibault.kine@laplateforme.io>
2022-08-01 11:04:09 +0200

    css

--- Libraries/Imports/footer-index.php
+++ Libraries/Imports/footer-index.php
@@ -17,6 +17,27 @@
             <a href="./Libraries/Views/contact.php"><img src="./Libraries/Style/Icons/support.png" alt=""></a>
         </div>
     </div>
+
+    <div class="contact-us">
+    <div class="lines-around">
+        <span class="line"></span>
+        <h1>Nos réseaux</h1>
+        <span class="line"></span>
+    </div>
+    <ul>
+        <li id="fb-icon">
+            <a href="">facebook</a>
+        </li>
+        <li id="ig-icon">
+            <a href="">instagram</a>
+        </li>
+        <li id="li-icon">
+            <a href="">linkedin</a>
+        </li>
+    </ul>
+
+    <h1><a href="./Libraries/Views/contact.php">Nous contacter</a></h1>    
+</div>
 </footer>
     
 </body>
